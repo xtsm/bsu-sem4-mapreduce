609c8cf0b7aa4b48df019f252541eefd72c9a63c
Stanislau Tsitsianok <tsmx@tut.by>
2020-05-05 16:55:10 +0300

    added handling errors appearing in forked process

--- process_unix.cpp
+++ process_unix.cpp
@@ -16,52 +16,79 @@ enum class ProcessState {
 class ProcessUnix : public Process {
  public:
   explicit ProcessUnix(const std::string& exec) :
-      exec_(exec), args_(), input_(), output_(), pid_(0), state_(ProcessState::CREATED) {}
+      exec_(exec), args_(), input_(), output_(), pid_(0),
+      state_(ProcessState::CREATED) {}
 
   void Run() override {
     if (state_ != ProcessState::CREATED) {
       throw std::runtime_error("can't run twice");
     }
+    int fd[2];
+    if (pipe2(fd, O_CLOEXEC) < 0) {
+      throw std::runtime_error("pipe2() failed");
+    }
     pid_t res = fork();
     if (res < 0) {
+      close(fd[0]);
+      close(fd[1]);
       throw std::runtime_error("fork() failed");
     }
     if (res == 0) {
-      std::vector<char*> argv;
-      argv.push_back(exec_.data());
-      for (auto& arg : args_) {
-        argv.push_back(arg.data());
-      }
-      argv.push_back(nullptr);
+      close(fd[0]);
+      try {
+        std::vector<char*> argv;
+        argv.push_back(exec_.data());
+        for (auto& arg : args_) {
+          argv.push_back(arg.data());
+        }
+        argv.push_back(nullptr);
 
-      if (input_.has_value()) {
-        int fd = open(input_->c_str(), O_RDONLY);
-        if (fd < 0) {
-          std::ostringstream ss;
-          ss << "opening \"" << *input_ << "\" for read failed";
-          throw std::runtime_error(ss.str());
+        if (input_.has_value()) {
+          int fd = open(input_->c_str(), O_RDONLY);
+          if (fd < 0) {
+            std::ostringstream ss;
+            ss << "opening \"" << *input_ << "\" for read failed";
+            throw std::runtime_error(ss.str());
+          }
+          if (dup2(fd, STDIN_FILENO) < 0) {
+            throw std::runtime_error("dup2() for stdin failed");
+          }
         }
-        if (dup2(fd, STDIN_FILENO) < 0) {
-          throw std::runtime_error("dup2() for stdin failed");
-        }
-      }
 
-      if (output_.has_value()) {
-        int fd = open(output_->c_str(), O_WRONLY | O_CREAT, 0644);
-        if (fd < 0) {
-          std::ostringstream ss;
-          ss << "opening \"" << *output_ << "\" for write failed";
-          throw std::runtime_error(ss.str());
+        if (output_.has_value()) {
+          int fd = open(output_->c_str(), O_WRONLY | O_CREAT, 0644);
+          if (fd < 0) {
+            std::ostringstream ss;
+            ss << "opening \"" << *output_ << "\" for write failed";
+            throw std::runtime_error(ss.str());
+          }
+          if (dup2(fd, STDOUT_FILENO) < 0) {
+            throw std::runtime_error("dup2() for stdout failed");
+          }
         }
-        if (dup2(fd, STDOUT_FILENO) < 0) {
-          throw std::runtime_error("dup2() for stdout failed");
-        }
-      }
 
-      execvp(exec_.c_str(), argv.data());
-      _exit(1);
+        execvp(exec_.c_str(), argv.data());
+        throw std::runtime_error("execvp() failed");
+      } catch (const std::exception& e) {
+        write(fd[1], e.what(), strlen(e.what()) + 1);
+        close(fd[1]);
+        _exit(0);
+      }
     } else {
       pid_ = res;
+      close(fd[1]);
+      std::vector<char> buf(256);
+      std::string exc_msg;
+      int cnt;
+      while ((cnt = read(fd[0], buf.data(), buf.size())) > 0) {
+        exc_msg.append(buf.begin(), buf.begin() + cnt);
+      }
+      close(fd[0]);
+      if (!exc_msg.empty()) {
+        std::ostringstream s;
+        s << "failed to create process: " << exc_msg;
+        throw std::runtime_error(s.str());
+      }
       state_ = ProcessState::RUNNING;
     }
   }
