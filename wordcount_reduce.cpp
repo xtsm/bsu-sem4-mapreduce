c0d1d6ed17992ce71ab6884d618c9d613541b17d
Stanislau Tsitsianok <tsmx@tut.by>
2020-05-10 12:57:25 +0300

    added key equality assert

--- wordcount_reduce.cpp
+++ wordcount_reduce.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <optional>
 
@@ -9,10 +10,14 @@ int main() {
   while (std::getline(std::cin, line)) {
     size_t tab_pos = line.find('\t');
     uint64_t count = std::stoull(line.substr(tab_pos + 1));
+    std::string new_key = line.substr(0, tab_pos);
     if (!key.has_value()) {
-      key = line.substr(0, tab_pos);
+      key = new_key;
+    } else if (*key != new_key) {
+      std::ostringstream ss;
+      ss << "got different keys in input: \"" << *key << "\", \"" << new_key << "\"";
+      throw std::runtime_error(ss.str());
     }
-    // TODO(xtsm) should I add key equality assert?
     sum += count;
   }
   if (key.has_value()) {
