86455f352a6f0dcabaaa3885697990589d3d21ab
Stanislau Tsitsianok <tsmx@tut.by>
2020-05-10 14:01:55 +0300

    moved reduce split and merge into separate functions

--- mapreduce.cpp
+++ mapreduce.cpp
@@ -9,6 +9,69 @@
 #include "include/process.h"
 #include "include/tmpdir.h"
 
+void SplitReduceInput(const std::filesystem::path& inpath,
+    const std::filesystem::path& dirpath,
+    std::unordered_map<size_t, std::vector<std::string>>& hashmap) {
+  std::ifstream fin(inpath);
+  if (!fin.is_open()) {
+    std::ostringstream ss;
+    ss << "failed to open " << inpath;
+    throw std::runtime_error(ss.str());
+  }
+  std::hash<std::string> hasher;
+  std::string line;
+  while (getline(fin, line)) {
+    size_t tab_pos = line.find('\t');
+    if (tab_pos == std::string::npos) {
+      continue;
+    }
+    std::string key = line.substr(0, tab_pos);
+    size_t hash = hasher(key);
+    auto& colls = hashmap[hash];
+    size_t coll_num = colls.size();
+    auto it = std::find(colls.begin(), colls.end(), key);
+    if (it == colls.end()) {
+      colls.push_back(key);
+    } else {
+      coll_num = it - colls.begin();
+    }
+    std::string fname_in = dirpath / (std::to_string(hash) + "_"
+        + std::to_string(coll_num) + "_in");
+    std::ofstream fout(fname_in, std::ios_base::out | std::ios_base::app);
+    if (!fout.is_open()) {
+      std::ostringstream ss;
+      ss << "failed to open " << fname_in;
+      throw std::runtime_error(ss.str());
+    }
+    fout << line << std::endl;
+    fout.close();
+  }
+  fin.close();
+}
+
+void MergeReduceOutput(const std::filesystem::path& outpath,
+    const std::filesystem::path& dirpath,
+    std::unordered_map<size_t, std::vector<std::string>>& hashmap) {
+  std::ofstream fout(outpath);
+  if (!fout.is_open()) {
+    std::ostringstream ss;
+    ss << "failed to open " << outpath;
+    throw std::runtime_error(ss.str());
+  }
+  for (const auto& kv : hashmap) {
+    for (size_t i = 0; i < kv.second.size(); i++) {
+      std::ifstream fin(dirpath / (std::to_string(kv.first) + "_"
+          + std::to_string(i) + "_out"));
+      std::string line;
+      while (getline(fin, line)) {
+        fout << line << std::endl;
+      }
+      fin.close();
+    }
+  }
+  fout.close();
+}
+
 int main(int argc, char** argv) {
   if (argc != 5) {
     std::cerr << "Usage: " << argv[0] << " <map|reduce> <exec> <input> <output>"
@@ -28,40 +91,8 @@ int main(int argc, char** argv) {
     }
   } else if (!strcmp(argv[1], "reduce")) {
     TmpDir tmpdir("mr_tmp");
-    std::ifstream fin(argv[3]);
-    if (!fin.is_open()) {
-      std::cerr << "failed to open " << argv[3] << std::endl;
-      return 1;
-    }
-    std::hash<std::string> hasher;
     std::unordered_map<size_t, std::vector<std::string>> hashmap;
-    std::string line;
-    while (getline(fin, line)) {
-      size_t tab_pos = line.find('\t');
-      if (tab_pos == std::string::npos) {
-        continue;
-      }
-      std::string key = line.substr(0, tab_pos);
-      size_t hash = hasher(key);
-      auto& colls = hashmap[hash];
-      size_t coll_num = colls.size();
-      auto it = std::find(colls.begin(), colls.end(), key);
-      if (it == colls.end()) {
-        colls.push_back(key);
-      } else {
-        coll_num = it - colls.begin();
-      }
-      std::string fname_in = tmpdir.GetPath() / (std::to_string(hash) + "_"
-          + std::to_string(coll_num) + "_in");
-      std::ofstream fout(fname_in, std::ios_base::out | std::ios_base::app);
-      if (!fout.is_open()) {
-        std::cerr << "failed to open " << fname_in << std::endl;
-        return 1;
-      }
-      fout << line << std::endl;
-      fout.close();
-    }
-    fin.close();
+    SplitReduceInput(argv[3], tmpdir.GetPath(), hashmap);
 
     std::vector<std::unique_ptr<Process>> reducers;
     for (const auto& kv : hashmap) {
@@ -69,9 +100,7 @@ int main(int argc, char** argv) {
         std::string fname_base = tmpdir.GetPath() / (std::to_string(kv.first)
             + "_" + std::to_string(i) + "_");
         reducers.push_back(Process::Create(argv[2]));
-        reducers.back()->SetInput(fname_base + "in");
-        reducers.back()->SetOutput(fname_base + "out");
-        reducers.back()->Run();
+        reducers.back()->Run(fname_base + "in", fname_base + "out");
       }
     }
     bool is_success_for_all = true;
@@ -84,22 +113,8 @@ int main(int argc, char** argv) {
       std::cerr << "at least one reducer has failed" << std::endl;
       return 1;
     }
-    std::ofstream fout(argv[4]);
-    if (!fout.is_open()) {
-      std::cerr << "failed to open " << argv[4] << std::endl;
-      return 1;
-    }
-    for (const auto& kv : hashmap) {
-      for (size_t i = 0; i < kv.second.size(); i++) {
-        fin.open(tmpdir.GetPath() / (std::to_string(kv.first) + "_"
-            + std::to_string(i) + "_out"));
-        while (getline(fin, line)) {
-          fout << line << std::endl;
-        }
-        fin.close();
-      }
-    }
-    fout.close();
+
+    MergeReduceOutput(argv[4], tmpdir.GetPath(), hashmap);
     return 0;
   } else {
     std::cerr << "unknown mode: " << argv[1] << std::endl;
